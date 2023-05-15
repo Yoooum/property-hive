f610f94a9f2136a3cf397f42e008edf48ac31bc5
Yoooum <akyomi@qq.com>
2023-05-15 11:01:03 +0800

    feat: getByName template

--- src/main/java/com/prprv/property/controller/sys/PermissionController.java
+++ src/main/java/com/prprv/property/controller/sys/PermissionController.java
@@ -1,10 +1,16 @@
 package com.prprv.property.controller.sys;
 
+import com.prprv.property.common.response.R;
 import com.prprv.property.controller.AbstractCrudController;
 import com.prprv.property.entity.sys.Permission;
 import com.prprv.property.repo.sys.PermissionRepository;
+import org.springframework.data.domain.Example;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 /**
  * @author Yoooum
@@ -15,5 +21,13 @@ public class PermissionController extends AbstractCrudController<Permission, Per
 
     protected PermissionController(PermissionRepository repository) {
         super(repository);
+    }
+
+    @GetMapping("/name/{name}")
+    public R<Optional<Permission>> getByName(@PathVariable String name) {
+        Permission permission = new Permission();
+        permission.setName(name);
+        Example<Permission> example = Example.of(permission);
+        return R.ok(this.repository.findOne(example));
     }
 }
