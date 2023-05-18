59fbff7c9d21a988a8dba31e0ee4c5dbf3b9a80e
phj233 <2780990934@qq.com>
2023-05-18 11:41:12 +0800

    :recycle: refactor: Optimize sending activation emails

--- src/main/java/com/prprv/property/service/EmailService.java
+++ src/main/java/com/prprv/property/service/EmailService.java
@@ -1,5 +1,9 @@
 package com.prprv.property.service;
 
+import com.prprv.property.common.response.E;
+import com.prprv.property.entity.sys.User;
+import com.prprv.property.exception.AppException;
+import com.prprv.property.repo.sys.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -7,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import java.security.SecureRandom;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -18,14 +23,31 @@ import java.util.concurrent.TimeUnit;
 public class EmailService {
     private final RedisTemplate<String, String> redisTemplate;
     private final RabbitTemplate rabbitTemplate;
+    private final UserRepository userRepository;
 
-    public void sendActivationEmail( String email, String activationCode) {
-
-        // 创建邮件消息
-        EmailMessage message = new EmailMessage(email, activationCode);
-
-        // 将消息发送到RabbitMQ队列中
-        rabbitTemplate.convertAndSend("email.queue", message);
+    public void sendActivationEmail(String email, String activationCode) {
+        if (userRepository.findByEmail(email).isEmpty()) {
+            throw new AppException(E.ACTIVATION_FAILED);
+        }
+        Optional<User> user = userRepository.findByEmail(email);
+        user.map(u -> {
+            if (u.getActivated()) {
+                throw new AppException(E.USER_ACTIVATED);
+            }
+            return u;
+        });
+        String code = redisTemplate.opsForValue().get(email + ":code");
+        if (code != null && !code.equals(activationCode)) {
+            throw new AppException(E.CODE_EXPIRED);
+        }
+        try {
+            // 创建邮件消息
+            EmailMessage message = new EmailMessage(email, activationCode);
+            // 将消息发送到RabbitMQ队列中
+            rabbitTemplate.convertAndSend("email.queue", message);
+        }catch (Exception e){
+            throw new AppException(E.EMAIL_SEND_FAILED);
+        }
     }
 
     public String generateActivationCode(String email) {
